acd7695c67d11cb7add2f480295e52617f0f9748
Claire Chung <35644336+haewonchung@users.noreply.github.com>
2022-01-17 14:51:20 +0900

    Modified index

--- new/static/js/scripts.js
+++ new/static/js/scripts.js
@@ -1,47 +1,52 @@
 /*!
-* Start Bootstrap - Stylish Portfolio v6.0.4 (https://startbootstrap.com/theme/stylish-portfolio)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-stylish-portfolio/blob/master/LICENSE)
-*/
-window.addEventListener('DOMContentLoaded', event => {
-
-    const sidebarWrapper = document.getElementById('sidebar-wrapper');
+ * Start Bootstrap - Stylish Portfolio v6.0.4 (https://startbootstrap.com/theme/stylish-portfolio)
+ * Copyright 2013-2021 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-stylish-portfolio/blob/master/LICENSE)
+ */
+window.addEventListener("DOMContentLoaded", (event) => {
+    const sidebarWrapper = document.getElementById("sidebar-wrapper");
     let scrollToTopVisible = false;
     // Closes the sidebar menu
-    const menuToggle = document.body.querySelector('.menu-toggle');
-    menuToggle.addEventListener('click', event => {
+    const menuToggle = document.body.querySelector(".menu-toggle");
+    menuToggle.addEventListener("click", (event) => {
         event.preventDefault();
-        sidebarWrapper.classList.toggle('active');
+        sidebarWrapper.classList.toggle("active");
         _toggleMenuIcon();
-        menuToggle.classList.toggle('active');
-    })
+        menuToggle.classList.toggle("active");
+    });
 
     // Closes responsive menu when a scroll trigger link is clicked
-    var scrollTriggerList = [].slice.call(document.querySelectorAll('#sidebar-wrapper .js-scroll-trigger'));
-    scrollTriggerList.map(scrollTrigger => {
-        scrollTrigger.addEventListener('click', () => {
-            sidebarWrapper.classList.remove('active');
-            menuToggle.classList.remove('active');
+    var scrollTriggerList = [].slice.call(
+        document.querySelectorAll("#sidebar-wrapper .js-scroll-trigger")
+    );
+    scrollTriggerList.map((scrollTrigger) => {
+        scrollTrigger.addEventListener("click", () => {
+            sidebarWrapper.classList.remove("active");
+            menuToggle.classList.remove("active");
             _toggleMenuIcon();
-        })
+        });
     });
 
     function _toggleMenuIcon() {
-        const menuToggleBars = document.body.querySelector('.menu-toggle > .fa-bars');
-        const menuToggleTimes = document.body.querySelector('.menu-toggle > .fa-times');
+        const menuToggleBars = document.body.querySelector(
+            ".menu-toggle > .fa-bars"
+        );
+        const menuToggleTimes = document.body.querySelector(
+            ".menu-toggle > .fa-times"
+        );
         if (menuToggleBars) {
-            menuToggleBars.classList.remove('fa-bars');
-            menuToggleBars.classList.add('fa-times');
+            menuToggleBars.classList.remove("fa-bars");
+            menuToggleBars.classList.add("fa-times");
         }
         if (menuToggleTimes) {
-            menuToggleTimes.classList.remove('fa-times');
-            menuToggleTimes.classList.add('fa-bars');
+            menuToggleTimes.classList.remove("fa-times");
+            menuToggleTimes.classList.add("fa-bars");
         }
     }
 
     // Scroll to top button appear
-    document.addEventListener('scroll', () => {
-        const scrollToTop = document.body.querySelector('.scroll-to-top');
+    document.addEventListener("scroll", () => {
+        const scrollToTop = document.body.querySelector(".scroll-to-top");
         if (document.documentElement.scrollTop > 100) {
             if (!scrollToTopVisible) {
                 fadeIn(scrollToTop);
@@ -53,17 +58,15 @@ window.addEventListener('DOMContentLoaded', event => {
                 scrollToTopVisible = false;
             }
         }
-    })
-})
-
+    });
+});
 
 function fileupload() {
-    
-    let file = $('#btn_upload')[0].files[0]
-    let form_data = new FormData()
+    let file = $("#btn_upload")[0].files[0];
+    let form_data = new FormData();
 
-    form_data.append("file_give", file)
-    console.log(form_data)
+    form_data.append("file_give", file);
+    console.log(form_data);
     $.ajax({
         type: "POST",
         url: "/fileupload",
@@ -72,10 +75,10 @@ function fileupload() {
         contentType: false,
         processData: false,
         success: function (response) {
-            alert(response["result"])
-            window.location.replace('/result')
+            // alert(response["result"])
+            window.location.replace("/result");
             // window.location.href
-        }
+        },
     });
 }
 
@@ -84,13 +87,13 @@ function fileupload() {
 //         type: 'GET',
 //         url: '/result',
 //         data: {},
-//         success: function (response) { 
+//         success: function (response) {
 //         }
 //     })
 // }
 
 function preview() {
-    let frame = $('#frame');
-    frame.src=URL.createObjectURL(target.files[0]);
-    frame.style.display = 'block';
+    let frame = $("#frame");
+    frame.src = URL.createObjectURL(target.files[0]);
+    frame.style.display = "block";
 }
